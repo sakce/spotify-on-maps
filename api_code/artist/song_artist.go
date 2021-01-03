98ae30a7caf5eb04c81b3859cb4065971fd4f60c
Kaloyan Videlov <kvidelov@protonmail.com>
2021-01-03 11:44:50 +0100

    Modified makefile and added player, artist and song.

---
+++ api_code/artist/song_artist.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/zmb3/spotify"
+)
+
+const redirectURI = "http://localhost:8080/callback"
+
+var (
+	auth  = spotify.NewAuthenticator(redirectURI, spotify.ScopeUserReadCurrentlyPlaying, spotify.ScopeUserReadPlaybackState, spotify.ScopeUserModifyPlaybackState)
+	ch    = make(chan *spotify.Client)
+	state = "abc123"
+)
+
+func main() {
+
+	var client *spotify.Client
+	http.HandleFunc("/callback", completeAuth)
+
+	go func() {
+		url := auth.AuthURL(state)
+		fmt.Println("Please log in to Spotify by visiting the following page in your browser:", url)
+
+		// wait for auth to complete
+		client = <-ch
+
+		// use the client to make calls that require authorization
+		user, err := client.CurrentUser()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("You are logged in as:", user.ID)
+
+		track, err := client.PlayerCurrentlyPlaying()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Artist: %s\n", track.Item.Artists[0].Name)
+	}()
+
+	http.ListenAndServe(":8080", nil)
+
+}
+
+func completeAuth(w http.ResponseWriter, r *http.Request) {
+	tok, err := auth.Token(state, r)
+	if err != nil {
+		http.Error(w, "Couldn't get token", http.StatusForbidden)
+		log.Fatal(err)
+	}
+	if st := r.FormValue("state"); st != state {
+		http.NotFound(w, r)
+		log.Fatalf("State mismatch: %s != %s\n", st, state)
+	}
+	// use the token to get an authenticated client
+	client := auth.NewClient(tok)
+	fmt.Fprintf(w, "Login Completed!")
+	ch <- &client
+}
